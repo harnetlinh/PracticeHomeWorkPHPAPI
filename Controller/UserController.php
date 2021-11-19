fc10d173512a8eca61a8492a160839d7b0a857d7
Ha Ngoc Linh <harnet.linh@gmail.com>
2021-11-19 23:30:04 +0700

    update file dir

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -3,10 +3,11 @@
 /**
  * UserController
  */
-class UserController{
+class UserController
+{
     private $requestMethod;
     private $user = null;
-    
+
     /**
      * __construct
      *
@@ -22,7 +23,7 @@ class UserController{
         $this->userPhone = $userPhone;
         $this->user = new User();
     }
-    
+
     /**
      * processRequest
      *
@@ -52,13 +53,14 @@ class UserController{
             echo $response['body'];
         }
     }
-        
+
     /**
      * get user by $_PUT['id'])
      *
      * @return bool
      */
-    private function updateUser(){
+    private function updateUser()
+    {
         // TODO create function update user
         // return true | false;
     }
@@ -68,17 +70,19 @@ class UserController{
      *
      * @return bool
      */
-    private function deleteUser(){
+    private function deleteUser()
+    {
         // TODO create function delete user
         // return true | false;
     }
-    
+
     /**
      * get user by $_GET['name']) and $_GET['phone']
      *
      * @return mixed
      */
-    private function getUser(){
+    private function getUser()
+    {
 
         $name = (isset($_GET['name']) && $_GET['name']) ? $_GET['name'] : '';
         $phone = (isset($_GET['phone']) && $_GET['phone']) ? $_GET['phone'] : '';
@@ -87,22 +91,22 @@ class UserController{
             $response['status_code_header'] = 'HTTP/1.1 200 OK';
             $response['body'] = $result['result'];
             return $response;
-        }else{
+        } else {
             $response['status_code_header'] = 'HTTP/1.1 500 Internal Server Error';
             $response['body'] = [];
             return $response;
         }
     }
 
-    
+
     /**
      * create new user by $_POST
      *
      * @return mixed
      */
     private function createUser()
-    {      
-        if (! $this->validatePerson()) {
+    {
+        if (!$this->validatePerson()) {
             return $this->unprocessableEntityResponse();
         }
         $file_name = $this->storeFile();
@@ -114,19 +118,19 @@ class UserController{
         $name = $_POST['name'];
         $phone = $_POST['phone'];
         $avatar = $file_name;
-        
+
         $result = $this->user->create($name, $phone, $avatar);
         if ($result) {
             $response['status_code_header'] = 'HTTP/1.1 201 Created';
             $response['body'] = 'success';
             return $response;
-        }else{
+        } else {
             $response['status_code_header'] = 'HTTP/1.1 500 Internal Server Error';
             $response['body'] = 'failed';
             return $response;
         }
     }
-    
+
     /**
      * validate for createUser() function
      *
@@ -134,54 +138,53 @@ class UserController{
      */
     private function validatePerson()
     {
-        if (! isset($_POST['name'])) {
+        if (!isset($_POST['name'])) {
             error_log('No name');
             return false;
         }
-        if (! isset($_POST['phone'])) {
+        if (!isset($_POST['phone'])) {
             error_log('No phone');
             return false;
         }
-        if (! isset($_FILES['avatar']['name'])) {
+        if (!isset($_FILES['avatar']['name'])) {
             error_log('No file');
             return false;
         }
         return true;
     }
-        
+
     /**
      * store avatar in /uploaded
      *
      * @return bool
      */
-    private function storeFile(){
+    private function storeFile()
+    {
         try {
             $fileName  =  $_FILES['avatar']['name'];
             $tempPath  =  $_FILES['avatar']['tmp_name'];
             $fileSize  =  $_FILES['avatar']['size'];
-            $upload_path = 'E:\CODE\300-Bai-Code-Thieu-Nhi\50-Bai-Code-PHP\API-sample\uploaded/'; // set upload folder path 
-            $fileExt = strtolower(pathinfo($fileName,PATHINFO_EXTENSION)); // get image extension
+            $upload_path = $_SERVER['DOCUMENT_ROOT'] . '\uploaded/'; // set upload folder path 
+            $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION)); // get image extension
             // valid image extensions
-            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); 
+            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif');
             // allow valid image file formats
-            if(in_array($fileExt, $valid_extensions))
-            {
+            if (in_array($fileExt, $valid_extensions)) {
+                $newFileName = date("Y-m-d-h-i") . '-num-' . rand(1, 999) . '.' . $fileExt; // create new file name
                 //check file not exist our upload folder path
-                if(!file_exists($upload_path . $fileName))
-                {
-                    if($fileSize < 5000000){
-                        $result = move_uploaded_file($tempPath, $upload_path . $fileName); // move file from system temporary path to our upload folder path 
+                if (!file_exists($upload_path . $newFileName)) {
+                    if ($fileSize < 5000000) {
+                        $result = move_uploaded_file($tempPath, $upload_path . $newFileName); // move file from system temporary path to our upload folder path 
                         if ($result) {
-                            return $fileName;
-                        }else{
+                            return $newFileName;
+                        } else {
                             return false;
                         }
                     }
-                }
-                else{
+                } else {
                     return false;
                 }
-            }else{
+            } else {
                 return false;
             }
         } catch (\Throwable $th) {
@@ -189,7 +192,7 @@ class UserController{
             return false;
         }
     }
-    
+
     /**
      * return unprocessable Entity Response
      *
@@ -203,7 +206,7 @@ class UserController{
         ]);
         return $response;
     }
-    
+
     /**
      * return not Found Response
      *
